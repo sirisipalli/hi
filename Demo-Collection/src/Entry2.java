582129e159252b0d01ca100c07359a5e452f1fe3
abhishek-ghode <abhishek.ghode@capgemini.com>
2018-04-25 17:07:32 +0530

    Added a demo on LinkedList (java.util)

---
+++ Demo-Collection/src/Entry2.java
@@ -0,0 +1,67 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class Entry2 {
+
+	public static void main(String[] args) {
+		
+		Collection c;
+		
+		LinkedList list = new LinkedList();
+	
+		c = (Collection)list;		//up-casting
+	
+		
+		char ch =  '\uFFFF';				;
+		
+	
+		System.out.println((double)ch);
+		
+		
+		
+		
+		
+		c.add("Welcome to DS");
+		c.add("We're using linked List");
+		
+		c.add("java is simple!");
+		c.add("JAVA IS SIMPLE!");
+		c.add("JAVA IS SIMPLE!");
+		c.add("JAVA IS SIMPLE!");
+		
+		c.add("We are using collection");
+		c.add("We're using type casting");
+		
+		
+		Iterator itr = c.iterator();
+		System.out.println(itr.getClass().getSimpleName());
+		while(itr.hasNext()){
+			String message = (String)itr.next();			// down-casting
+			System.out.println(message);
+		}
+		
+		TreeSet tree = new TreeSet();
+		
+		tree.addAll(c);
+		
+		itr = tree.iterator();
+		
+		System.out.println();System.out.println();System.out.println();
+		System.out.println(itr.getClass().getSimpleName());
+		while(itr.hasNext()){
+			String message = (String)itr.next();			// down-casting
+			System.out.println(message);
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
